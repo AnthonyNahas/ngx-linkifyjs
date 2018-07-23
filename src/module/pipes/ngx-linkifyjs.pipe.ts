98f4b7dfcb04d2f67ce93d31e6f4aed6bfe94199
Anthony Nahas <anthony.na@hotmail.de>
2018-07-23 21:55:38 +0200

    feat(package): added `linkify` pipe

---
+++ src/module/pipes/ngx-linkifyjs.pipe.ts
@@ -0,0 +1,14 @@
+import {Pipe, PipeTransform} from '@angular/core';
+// @ts-ignore
+import linkifyStr from 'linkifyjs/string';
+
+@Pipe({
+  name: 'linkify'
+})
+export class NgxLinkifyjsPipe implements PipeTransform {
+
+  transform(value: any, args?: any): string {
+    return value ? linkifyStr(value, {target: '_system'}) : value;
+  }
+
+}

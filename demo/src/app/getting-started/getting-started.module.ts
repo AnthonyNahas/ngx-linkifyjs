45f1a0181ff794cf2611ea99e547a7025488b493
Anthony Nahas <anthony.na@hotmail.de>
2018-07-25 12:48:34 +0200

    demo(app): added getting started component

--- demo/src/app/getting-started/getting-started.module.ts
+++ demo/src/app/getting-started/getting-started.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GettingStartedComponent } from './getting-started.component';
-import { GettingStartedRoutingModule } from './getting-started-routing.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GettingStartedComponent} from './getting-started.component';
+import {GettingStartedRoutingModule} from './getting-started-routing.module';
+import {MarkdownModule} from 'ngx-markdown';
 
 @NgModule({
   imports: [
     CommonModule,
+    MarkdownModule.forChild(),
     GettingStartedRoutingModule
   ],
   declarations: [GettingStartedComponent],
 })
-export class GettingStartedModule { }
+export class GettingStartedModule {
+}

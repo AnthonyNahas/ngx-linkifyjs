f8bcdfffbcd6ef3c70b50fa0bab50eb9fd52a836
Anthony Nahas <anthony.na@hotmail.de>
2018-10-09 12:10:16 +0300

    demo(app): support of ssr and pre rendering

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'getting-started',
-        loadChildren: 'app/getting-started/getting-started.module#GettingStartedModule'
-    }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'getting-started',
+    loadChildren: 'app/getting-started/getting-started.module#GettingStartedModule'
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {initialNavigation: 'enabled'})],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}

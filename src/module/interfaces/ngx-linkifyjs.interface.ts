53ea407bf0f4e5bf814030692e257abbe98b6755
Anthony Nahas <anthony.na@hotmail.de>
2019-01-10 17:51:06 +0100

    feat(package): added `NgxLinkifyOptions` interface

--- src/module/interfaces/ngx-linkifyjs.interface.ts
+++ src/module/interfaces/ngx-linkifyjs.interface.ts
@@ -8,3 +8,17 @@ export interface NgxLinkifyjsConfig {
   enableHash?: boolean,
   enableMention?: boolean
 }
+
+export interface NgxLinkifyOptions {
+  attributes?: any,
+  className?: string,
+  defaultProtocol?: string,
+  events?: any,
+  ignoreTags?: Array<any>,
+  nl2br?: boolean,
+  tagName?: string,
+  target?: { url: string },
+  validate?: boolean,
+  format?(value: any, type: any): any,
+  formatHref?(href: any, type: any): any,
+}

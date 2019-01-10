5f56548b9f56cd0166c230db8b64a380c03cb8d8
Anthony Nahas <anthony.na@hotmail.de>
2019-01-10 18:28:19 +0100

    docs(package): added documentation to the `NgxLinkifyjsService`

--- src/module/service/ngx-linkifyjs.service.ts
+++ src/module/service/ngx-linkifyjs.service.ts
@@ -9,15 +9,31 @@ export class NgxLinkifyjsService {
   constructor() {
   }
 
+  /**
+   * Convert the passed text as a string to an appropriate url
+   *
+   * @param text - the string to convert
+   * @param options - options to pass it to the linkifyjs library
+   */
   linkify(text: string, options?: NgxLinkifyOptions): string {
     // @ts-ignore
     return text.linkify(options);
   }
 
+  /**
+   * Find any links in a given text as a string
+   *
+   * @param text - the string to find some links
+   */
   find(text: string): Array<Link> {
     return linkify.find(text);
   }
 
+  /**
+   * Test if a given value or array of values are links
+   *
+   * @param value - the value to test
+   */
   test(value: string | string[]): boolean {
     if (typeof value === 'string') {
       return linkify.test(value);
